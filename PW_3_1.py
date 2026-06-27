d88effd5883d0eb428a037b044f3155745c1f940
MaksymReznik-V <mov10112007@gmail.com>
2026-06-27 14:02:52 +0300

    Update PW_3_1.py

--- PW_3_1.py
+++ PW_3_1.py
@@ -14,14 +14,18 @@ def process_file(item: Path, dist_path: Path) -> None:
     copy_path = full_path / item.name
     shutil.copy2(item, copy_path)
 
+def process_directory(directory: Path, dist_path: Path, executor):
+    for item in directory.iterdir():
+        if item.is_file():
+            executor.submit(process_file, item, dist_path)
+        elif item.is_dir():
+            executor.submit(process_directory, item, dist_path, executor)
 
 def sorting_junk(directory_path: Path, dist_path: Path) -> None:
     dist_path.mkdir(exist_ok=True)
 
     with ThreadPoolExecutor(max_workers=4) as executor:
-        for item in directory_path.rglob("*"):
-            if item.is_file():
-                executor.submit(process_file, item, dist_path)
+        process_directory(directory_path, dist_path, executor)
 
 
 if __name__ == '__main__':
@@ -32,7 +36,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 2:
         dist_path = Path(sys.argv[2])
     else:
-        dist_path = Path('Dist')
+        dist_path = Path('dist')
 
     sorting_junk(source_path, dist_path)
 
